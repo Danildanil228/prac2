a8846dcc019c127ae845f3884d2de644da4ef5d7
Danil <d_silchenkov@mail.ru>
2024-10-29 16:22:44 +0300

    Добавьте файлы проекта.

---
+++ prac2/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Windows;
+
+namespace prac2
+{
+    public partial class App : Application
+    {
+        private const int MaxInstances = 3;
+        private static Semaphore _semaphore;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            bool createdNew;
+            _semaphore = new Semaphore(MaxInstances, MaxInstances, "prac2_LimitedInstances", out createdNew);
+
+            if (!createdNew)
+            {
+                MessageBox.Show("Достигнуто максимальное количество запущенных копий приложения.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                Shutdown();
+            }
+            else
+            {
+                base.OnStartup(e);
+            }
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            _semaphore?.Release();
+            base.OnExit(e);
+        }
+    }
+}
